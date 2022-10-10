156d8ab664b600c511f3dfed869de513fa802799
김정윤 <indl1670@gmail.com>
2022-10-10 19:00:44 +0900

    BOJ 11659 구간 합 구하기4

--- BOJ/BOJ11659_구간합구하기4.java
+++ BOJ/BOJ11659_구간합구하기4.java
@@ -18,14 +18,14 @@ public class BOJ11659_구간합구하기4 {
 		
 		st = new StringTokenizer(br.readLine());
 		for (int i = 1; i <= N; i++) {
-			nums[i] = nums[i-1] + Integer.parseInt(st.nextToken());
+			nums[i] = nums[i-1] + Integer.parseInt(st.nextToken()); // 구간 합 구하기
 		}
 		
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int start = Integer.parseInt(st.nextToken());
 			int end = Integer.parseInt(st.nextToken());
-			sb.append(nums[end] - nums[start - 1]).append("\n");
+			sb.append(nums[end] - nums[start - 1]).append("\n"); // 결과 도출
 		}
 		
 		System.out.println(sb.toString());
